093d753d19ed7e9f8736eeda5208f4eff8f7b2f4
gpauta <pautacreativadevelop@gmail.com>
2018-11-18 11:46:56 -0600

    email cambios fisicos

---
+++ hitazos/servicios/baseurl.php
@@ -0,0 +1,6 @@
+<?php
+if($_SERVER['SERVER_NAME'] == 'apps.pautacreativatemporales.com.mx')
+  $baseurl = "http://apps.pautacreativatemporales.com.mx/oster/homeproducts/servicios";
+else
+  $baseurl = "https://www.homeproductslatam.com.mx/servicios";
+?>
---
+++ hitazos/servicios/email-conf.php
@@ -0,0 +1,22 @@
+<?php
+
+$mail->IsSMTP();                           // tell the class to use SMTP
+$mail->SMTPAuth   = false;                  // enable SMTP authentication
+//$mail->SMTPDebug = 1;
+//$mail->SMTPSecure = "ssl";
+$mail->Port		  = "25";                    // set the SMTP server port
+$mail->Host       = "172.16.120.17"; // SMTP server
+//$mail->Host       = "172.16.120.17";
+//$mail->Host       = "69.49.115.72";
+//$mail->Username   = "slazo@pautacreativa.com.mx";     // SMTP server username
+//$mail->Password   = "kb#Zq83mIXS)";            // SMTP server password
+
+//$mail->IsSendmail();  // tell the class to use Sendmail
+
+//$mail->AddReplyTo("$email",utf8_decode("$nombre"));
+
+$mail->From       = "slazo@pautacreativa.com.mx";
+//$mail->From       = "slazo@pautacreativa.com.mx";
+$mail->FromName   = utf8_decode("Oster Home Products");
+
+?>
